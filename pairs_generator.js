33007881ff41f1b787f3349d2176c5cd1a9bf3f6
Alex Georoceanu <georoceanu@ro.ibm.com>
2015-12-14 10:48:51 +0200

    Optimized pairs generator

--- pairs_generator.js
+++ pairs_generator.js
@@ -14,23 +14,24 @@ function generate(participants) {
     var pick_sender = random_safe(participants.length);
     var pick_receiver = random_safe(participants_copy.length);
 
-    if (participants.length === 2) {
-      // when only two persons are left, pick manually the only other option in case of
-      if (participants[pick_sender].email === participants_copy[pick_receiver].email) {
-        // it suffices to just reverse the pick_receiver from 0 to 1 and from 1 to 0
+    // when only two persons are left, pick manually the only viable option
+    // if it wasn't already picked
+    if (participants.length === 2 &&
+        (participants[pick_sender].email === participants_copy[pick_receiver].email
+         || participants[~~!pick_sender].email === participants_copy[~~!pick_receiver].email)
+       ) {
         pick_receiver = ~~!pick_receiver;
-      } else if (participants[~~!pick_sender].email === participants_copy[~~!pick_receiver].email) {
-        // if the remainder would be shit, draw again
-        continue;
-      }
-    } else if ( participants[pick_sender].email === participants_copy[pick_receiver].email ) {
+    } else if (participants[pick_sender].email === participants_copy[pick_receiver].email) {
       // if we chose the same person, skip
       continue;
     }
 
     var pick = {
       sender: participants.splice(pick_sender, 1)[0],
-      receiver: participants_copy.splice(pick_receiver, 1)[0]
+      receiver: participants_copy.splice(pick_receiver, 1)[0],
+      toString() {
+        return `${this.sender.name} ⇾ ${this.receiver.name}`;
+      }
     };
 
     present_pairs.push(pick);
